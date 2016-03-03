e2fab8d9093e36e4ea78fb9d2e7546ba0bac9ccc
Kari Hormi <hormi.kari@gmail.com>
2016-03-03 14:18:38 +0200

    cleanup

--- header/MainWindow.h
+++ header/MainWindow.h
@@ -50,7 +50,6 @@ private slots:
     void showPropertyDockActionSlot(bool checked);
 
     bool tabCloseRequestedSlot(int index);
-    void tabBarDoubleClicked(int index);
 
     void setPropertyBrowser();
 
